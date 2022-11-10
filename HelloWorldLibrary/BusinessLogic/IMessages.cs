8c662dcbe03e08df501eddb9d395ec8cbb87a274
zzzgi <d.t.acc@outlook.com>
2022-11-10 12:45:06 +0300

    Added nuget package Microsoft.Extensions.Logging.Abstractions for ILogger class -Added Models with CustomText class -Added Business Logic with Messages class and IMessage interface

---
+++ HelloWorldLibrary/BusinessLogic/IMessages.cs
@@ -0,0 +1,7 @@
+namespace HelloWorldLibrary.BusinessLogic
+{
+    public interface IMessages
+    {
+        string Greeting(string language);
+    }
+}
---
+++ HelloWorldLibrary/Models/CustomText.cs
@@ -0,0 +1,8 @@
+namespace HelloWorldLibrary.Models
+{
+    public class CustomText
+    {
+        public string? Language { get; set; }
+        public Dictionary<string, string>? Translations { get; set; }
+    }
+}
